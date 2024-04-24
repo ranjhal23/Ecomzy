20a6d606ae5eacd8b939eb3258be816ca49f778a
ranjhal23 <ranjhal2003@gmail.com>
2024-04-25 00:06:42 +0530

    final

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,5 +1,19 @@
-const App = () => {
-  return <>Shopping Cart</>;
-};
+import Navbar from "./components/Navbar"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Cart from "./pages/Cart"
 
+function App(){
+  return(
+    <div>
+      <div>
+        <Navbar/>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+      </Routes>
+    </div>
+  )
+}
 export default App;
